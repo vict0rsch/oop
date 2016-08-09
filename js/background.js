7e86cc58a61e8370781529db21ed4b4a1e4e545e
vict0rsch <vs.onphone@gmail.com>
2016-08-09 18:40:08 +0200

    alerting current tabs url not working

--- js/background.js
+++ js/background.js
@@ -5,4 +5,24 @@ $(function(){
     //     console.log('jjj')
     // }, 3000)
     console.log('james');
+
+    function get_url_tab(onglet){
+        alert(onglet.url + "\n" + localStorage["nbOfTime"]);
+        return onglet.url;
+    }
+
+    function get_url_window(fenetre){
+        chrome.tabs.getSelected(fenetre.id, function(onglet){
+            get_url_tab(onglet);
+        })
+    }
+
+    chrome.windows.getLastFocused(function(fenetre){
+        var current_url = get_url_window(fenetre);
+    })
+
+    document.addEventListener('visibilitychange', function(){
+        alert('change') // change tab text for demo
+    })
+
 });
