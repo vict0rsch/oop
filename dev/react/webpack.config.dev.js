28b6bcdbb346e571e437dd9c601d5d6729368be3
vict0rsch <vs.onphone@gmail.com>
2017-10-10 15:21:46 +0200

    untrack node modules

---
+++ dev/react/webpack.config.dev.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    // js
+    {
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'client')
+    },
+    // CSS
+    { 
+      test: /\.css$/, 
+      include: path.join(__dirname, 'client'),
+      loader: 'style-loader!css-loader!stylus-loader'
+    }
+    ]
+  }
+};
