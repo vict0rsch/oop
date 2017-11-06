f903ddcaa3e45f639d6cbe91a3c8ffc5eef91adc
vict0rsch <vs.onphone@gmail.com>
2017-11-06 15:48:32 +0100

    prettify + redux-persist v5 imports

--- dev/react/src/store.js
+++ dev/react/src/store.js
@@ -1,29 +1,24 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
-import {autoRehydrate, persistStore} from 'redux-persist';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 import { createBrowserHistory, createHashHistory } from 'history';
-
-
-// import the root reducer
 import rootReducer from './reducers/index';
+
+
 
 const browserHistory = createBrowserHistory();
 const hashHistory = createHashHistory();
-
-const USE_BROWsER_HISTORY = false;
-
+const USE_BROWSER_HISTORY = false;
 let _history;
-if (USE_BROWsER_HISTORY) {
+if (USE_BROWSER_HISTORY) {
     _history = browserHistory;
 } else {
     _history = hashHistory;
 }
-
 export const history = _history;
 
-const middleware = routerMiddleware(history);
 
-// create an object for the default data
 const defaultState = {
     data: {},
     dataIsAvailable: false,
@@ -35,13 +30,13 @@ const defaultState = {
     }
 };
 
-const enhancers = compose(autoRehydrate(), applyMiddleware(middleware),
+
+const middleware = routerMiddleware(history);
+const enhancers = compose(applyMiddleware(middleware),
     window.devToolsExtension ? window.devToolsExtension() : f => f
 );
-
 const store = createStore(rootReducer, defaultState, enhancers);
 
-persistStore(store);
 
 // By default reducers are not hot reloaded, only components
 // To make them hot reloadable : 
