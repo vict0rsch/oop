a0af1653026414595510b1c42a71045fa8063e0d
vict0rsch <vs.onphone@gmail.com>
2017-11-09 12:45:27 +0100

    now adds the md file to the appropriate global_locale.json keys

--- react/transform_md.py
+++ react/transform_md.py
@@ -1,18 +1,61 @@
+import json
+
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
+
+
+def update_locale(file_sets):
+    """
+        Takes a text file, reformat it in 1 line (with "\n"s ) and adds it to 
+        global.locale.json under the appropriate keys. Keys are created on the 
+        fly if they do not exist (see nested_set() )
+
+        Args:
+            file_sets: dict, keys 'names' and 'locale_keys'
+                names is the list of files to be 1-lined and added to global.locale.json's 'locale_keys'
+        Returns:
+            None: overwrites globale_locale_location
+
+    """
+
+    globale_locale_location = './src/static/texts/global.locale.json'
+    text_location = './src/static/texts/'
+
+    with open(globale_locale_location) as f:
+        global_locale = json.load(f)
+
+    for file_set in file_sets:
+        new_value = []
+        for file_name in file_set['names']:
+            print(file_name, '...', end='')
+            myFile = text_location + file_name
+            new_lines = ['"']
+
+            with open(myFile + '.md', 'r') as f:
+                lines = f.readlines()
+
+            for l in lines:
+                new_lines.append(
+                    l.replace("\n", "\\n")
+                )
+            new_lines.append('"')
+            new_line = ''.join(new_lines)
+            new_value.append(new_line)
+        nested_set(global_locale, file_set['locale_keys'], new_value)
+
+    with open(globale_locale_location, 'w') as f:
+        json.dump(global_locale, f)
+
 if __name__ == '__main__':
-    for file_title in ['intentFr', 'intentEn']:
-        print(file_title, '...', end='')
-        myFile = './src/static/texts/' + file_title
-        newLines = ["const text = ["]
 
-        with open(myFile + '.md', 'r') as f:
-            lines = f.readlines()
+    file_sets = [
+        {
+            'names': ['intent/intentEn', 'intent/intentFr'],
+            'locale_keys': ['home', 'intentPaperMd']
+        },
+    ]
 
-        for l in lines:
-            newLines.append(
-                "'" + l.replace("'", "\\'").replace("\n", "\\n") + "',\n"
-            )
-        newLines.append('].join("");\n\nexport default text;')
-
-        with open(myFile + '.js', 'w') as f:
-            f.writelines(newLines)
-        print(' done')
+    update_locale(file_sets)
