5d6a9778b719cd50815dae5eb8031ae51147c754
vict0rsch <vs.onphone@gmail.com>
2017-11-09 18:37:08 +0100

    fix fetch + update data

--- react/public/js/updateDataBackground.js
+++ react/public/js/updateDataBackground.js
@@ -1,31 +1,31 @@
 function updateData() {
     if (localStorage.dataTimestamp) {
-        const ts = parseInt(localStorage.dataTimestamp, 10);
-        const ts2 = Math.round((new Date()).getTime() / 1000);
+        var ts = parseInt(localStorage.dataTimestamp, 10);
+        var ts2 = Math.round((new Date()).getTime() / 1000);
 
-        const checkEvery = 20;//3600 * 24; // 1 day
-
-        if (ts2 - ts > checkEvery) {
-            console.log('Looking for DB Update...');
-            axios.get('http://oop-pro.herokuapp.com/db_meta_data').then(
-                (response) => {
-                    console.log('Success (updating data)');
-                    const dbMetaData = response.data;
-                    const oldMetaData = localStorage.dbMetaData ? JSON.parse(localStorage.dbMetaData) : undefined;
-                    if (oldMetaData === undefined || oldMetaData.version < dbMetaData.version){
-                        fetchData();
-                        localStorage.dbMetaData = JSON.stringify(dbMetaData);
-                    }
-                    localStorage.dataTimestamp = Math.round((new Date()).getTime() / 1000);
-                },
-                (error) => {
-                    console.log('Server Error  (updating data)');
-                    console.log(error);
-                }).catch(
-                (error) => {
-                    console.log('Catching JS Error (updating data)');
-                    console.log(error);
-                });
-        }
+        var checkEvery = 20;//3600 * 24; // 1 day
     }
+    if ((!localStorage.dataTimestamp || ts2 - ts > checkEvery) && localStorage.fetchingData !== 'true') {
+        console.log('Looking for DB Update...');
+        axios.get('http://oop-pro.herokuapp.com/db_meta_data').then(
+            (response) => {
+                console.log('Success (getting metadata data)');
+                const dbMetaData = response.data;
+                const oldMetaData = localStorage.dbMetaData ? JSON.parse(localStorage.dbMetaData) : undefined;
+                if (oldMetaData === undefined || oldMetaData.version < dbMetaData.version) {
+                    fetchData();
+                    localStorage.dbMetaData = JSON.stringify(dbMetaData);
+                }
+                localStorage.dataTimestamp = Math.round((new Date()).getTime() / 1000);
+            },
+            (error) => {
+                console.log('Server Error  (getting metadata data)');
+                console.log(error);
+            }).catch(
+            (error) => {
+                console.log('Catching JS Error (getting metadata data)');
+                console.log(error);
+            });
+    }
+
 }
