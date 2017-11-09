13febcb12d66e9b9a712078b62f1eebba13f60fa
vict0rsch <vs.onphone@gmail.com>
2017-11-09 17:22:37 +0100

    working changelanguage setting

--- react/src/components/Graph/Graph.js
+++ react/src/components/Graph/Graph.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../../actions/actionCreators';
 import mapStateToProps from '../../store/defaultMapStateToProps';
+import { setActiveLanguage } from 'react-localize-redux';
+
 
 import CytoContainer from './CytoContainer';
 import Waiting from '../Waiting';
@@ -19,9 +21,13 @@ class _Graph extends React.Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(actionCreators, dispatch);
+  return bindActionCreators(
+    {
+      ...actionCreators,
+      setActiveLanguage
+    },
+    dispatch);
 }
-
 const Graph = connect(mapStateToProps, mapDispatchToProps)(_Graph);
 
 export default Graph;
