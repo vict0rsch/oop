83ae9567181c43dc218077e794692b4ae6e4978b
vict0rsch <vs.onphone@gmail.com>
2017-11-09 12:46:27 +0100

    use global.locale and translate

--- react/src/components/Home/Contact/ContactPaper.js
+++ react/src/components/Home/Contact/ContactPaper.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
 import MarkdownPaper from '../MarkdownPaper';
 
-const enMdSource = [
-    '**Get in touch**:\n [(Google Form)](https://goo.gl/forms/Zzj9WSmImS7iIKJM2)\n',
-    '\n',
-    '**Suggest Edit**:\n [(Google Form)](https://goo.gl/forms/xuS5Jl4ufXFQDrIl1)\n',
-    '\n',
-    '**Github**: [Frontend](https://github.com/vict0rsch/oop) | [Backend](https://github.com/vict0rsch/oop-back) \n',
-].join("");
-
-const frMdSource = [
-    '**Contactez-nous**:\n [(Google Form)](https://goo.gl/forms/Zzj9WSmImS7iIKJM2)\n',
-    '\n',
-    '**Suggérer une modification**:\n [(Google Form)](https://goo.gl/forms/xuS5Jl4ufXFQDrIl1)\n',
-    '\n',
-    '**Github**: [Frontend](https://github.com/vict0rsch/oop) | [Backend](https://github.com/vict0rsch/oop-back) \n',
-].join("");
-
 class ContactPaper extends Component {
 
     
@@ -26,8 +10,7 @@ class ContactPaper extends Component {
             <div style={{textAlign:'center'}}>
                 <MarkdownPaper
                     {...this.props}
-                    enMdSource={enMdSource}
-                    frMdSource={frMdSource}
+                    source={this.props.translate('home.contactPaperMd')}
                     toggle={this.props.toggleContact}
                 />
             </div>
