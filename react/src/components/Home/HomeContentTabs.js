3b78f87508198adf3019ec389dcf329d18a470d9
vict0rsch <vs.onphone@gmail.com>
2017-11-14 11:07:41 +0100

    improve icons

--- react/src/components/Home/HomeContentTabs.js
+++ react/src/components/Home/HomeContentTabs.js
@@ -4,9 +4,10 @@ import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import SearchIcon from 'material-ui-icons/Search';
-import SettingsIcon from 'material-ui-icons/Settings';
-import ChatIcon from 'react-icons/lib/md/chat';
-import BulbIcon from 'react-icons/lib/go/organization';
+import SettingsIcon from 'react-icons/lib/go/settings';
+import ContactIcon from 'react-icons/lib/go/mail';
+import AboutIcon from 'react-icons/lib/go/organization';
+import ExtensionIcon from 'react-icons/lib/go/package';
 
 
 const styles = theme => ({
@@ -14,6 +15,9 @@ const styles = theme => ({
     flexGrow: 1,
     marginTop: theme.spacing.unit * 3,
   },
+  labelContainer: {
+    fontSize: '0.1em',
+  }
 });
 
 class HomeContentTabs extends React.Component {
@@ -28,48 +32,80 @@ class HomeContentTabs extends React.Component {
   };
 
   render() {
+
+    let tabs = ["search_bar", "intent", "contact", "settings"];
+    if (this.props.clientType !== "chromeExtension") {
+      tabs = [
+        ...tabs.slice(0, 2),
+        "extension",
+        ...tabs.slice(2)
+      ];
+    }
+
     const { classes } = this.props;
     const iconStyle = {
       width: "25px",
       height: "25px"
     };
     const icons = {
-      'search_bar': <SearchIcon style={iconStyle}/>,
-      'intent': <BulbIcon style={iconStyle}/>,
-      'contact': <ChatIcon style={iconStyle}/>,
-      'settings': <SettingsIcon style={iconStyle}/>
-    }
+      'search_bar': <SearchIcon style={iconStyle} />,
+      'intent': <AboutIcon style={iconStyle} />,
+      'contact': <ContactIcon style={iconStyle} />,
+      'settings': <SettingsIcon style={iconStyle} />,
+      'extension': <ExtensionIcon style={iconStyle} />
+    };
+
+    const tabStyle = this.props.clientType !== 'chromeExtension' ?
+      {
+        width: '100px'
+      }
+      :
+      {
+        width: '140px'
+      };
+
+    const labelStyle = this.props.clientType !== 'chromeExtension' ?
+      {
+        fontSize: '0.7em'
+      }
+      :
+      {
+        fontSize: '1em'
+      };
 
     return (
       <Paper className={classes.root}>
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            centered
-            scrollable={window.innerWidth < 630}
-            scrollButtons="auto"
-          >
-            {["search_bar", "intent", "contact", "settings"].map(
-              (v, k) => {
-                return <Tab
-                  key={'tab' + k}
-                  label={this.props.translate("home.tabs." + v)}
-                  icon={icons[v]}
-                  value={v}
-                  style={{width:'130px'}}
-                />;
-              }
-            )}
-          </Tabs>
-        </Paper>
-        );
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+          scrollable={window.innerWidth < 630}
+          scrollButtons="auto"
+        >
+          {tabs.map(
+            (v, k) => {
+              return <Tab
+                className={classes.labelContainer}
+                key={'tab' + k}
+                label={
+                  <span style={labelStyle}>{this.props.translate("home.tabs." + v)}</span>
+                }
+                icon={icons[v]}
+                value={v}
+                style={tabStyle}
+              />;
+            }
+          )}
+        </Tabs>
+      </Paper>
+    );
   }
 }
 
 HomeContentTabs.propTypes = {
-          classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(HomeContentTabs);
