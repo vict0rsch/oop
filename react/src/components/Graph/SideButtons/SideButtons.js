36a9be40fd8a2720c26cb4ec977eaf79d1cbb0a3
vict0rsch <vsch@protonmail.com>
2017-11-21 14:50:51 +0100

    include navigation side buttons if not mobile

--- react/src/components/Graph/SideButtons/SideButtons.js
+++ react/src/components/Graph/SideButtons/SideButtons.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import SearchButton from './SearchButton';
 import HomeButton from './HomeButton';
+import RefreshButton from './RefreshButton';
+import PreviousButton from './PreviousButton';
+import NextButton from './NextButton';
 
 class SideButtons extends Component {
     render() {
@@ -21,6 +24,9 @@ class SideButtons extends Component {
             <div style={sideButtonDivStyle}>
                 <HomeButton {...this.props} />
                 <SearchButton {...this.props} />
+                {this.props.clientType !== 'mobile' && <RefreshButton {...this.props} />}
+                {this.props.clientType !== 'mobile' && <PreviousButton {...this.props} />}
+                {this.props.clientType !== 'mobile' && <NextButton {...this.props} />}
             </div>
         );
     }
