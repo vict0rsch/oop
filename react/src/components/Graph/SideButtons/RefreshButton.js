f9c55dce76506b8bd5a6d4e0205e9a39941b3d0a
vict0rsch <vsch@protonmail.com>
2017-11-21 14:50:28 +0100

    refresh = rerender not reload
    Former-commit-id: 7824c2caa0ad96f5bf4a59ac2e6b6e16c96d2de1

---
+++ react/src/components/Graph/SideButtons/RefreshButton.js
@@ -0,0 +1,57 @@
+// @flow weak
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+import ResetIcon from 'material-ui-icons/Autorenew';
+import Tooltip from 'material-ui/Tooltip';
+
+
+
+const styles = theme => ({
+    button: {
+        height: '50px',
+        width: '50px',
+        display: 'block',
+        margin: '20px',
+        verticalAlign: 'middle'
+    },
+    icon: {
+        height: '30px',
+        width: '30px',
+    }
+});
+
+class HomeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.reRenderGraph();
+    }
+
+    render() {
+
+        return (
+            <Tooltip
+                id="tooltip-ResetButton"
+                title={this.props.translate('graph.sideButtons.resetTooltip')}
+                placement="bottom"
+                style={{ textAlign: 'center' }}
+            >
+                <Button raised fab className={this.props.classes.button} onClick={this.handleClick}>
+                    <ResetIcon className={this.props.classes.icon} />
+                </Button>
+            </Tooltip>
+        );
+    }
+}
+
+HomeButton.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HomeButton);
