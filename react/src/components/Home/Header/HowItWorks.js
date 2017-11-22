95d2c3513ce32adf7e23d4b75a71fdc2f2fbad03
vict0rsch <vsch@protonmail.com>
2017-11-22 16:11:06 +0100

    add setTimeout
    Former-commit-id: 13f4d04fce6d6a959cc961033de859f277eb7bf6

--- react/src/components/Home/Header/HowItWorks.js
+++ react/src/components/Home/Header/HowItWorks.js
@@ -3,10 +3,17 @@ import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui-icons/HelpOutline';
 
 export default class HowItWorks extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        setTimeout(this.props.toggleHowItWorks, 100)
+    }
     render() {
         return (
             <IconButton
-                onClick={this.props.toggleHowItWorks}
+                onClick={this.handleClick}
                 style={{ height: '100%' }}>
                 <Icon />
             </IconButton>
