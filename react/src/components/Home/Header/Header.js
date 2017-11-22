e8651928f1a4c6e73af5fac1224d7bc6f5732c37
vict0rsch <vsch@protonmail.com>
2017-11-22 16:10:47 +0100

    style
    Former-commit-id: 0a6fccf9d06f506713d83fb0584eb083425a1d73

--- react/src/components/Home/Header/Header.js
+++ react/src/components/Home/Header/Header.js
@@ -4,17 +4,40 @@ import HowItWorks from "./HowItWorks";
 export default class Header extends Component {
 
     render() {
-        const style = {
+        const parentDivstyle = {
             ...this.props.style,
-            height: '100px'
+            height: this.props.clientType === 'mobile' ? '150px' : '100px',
         };
+        const titleStyle = {
+            display: 'inline-block', 
+            height: '100%', 
+            float: 'left'
+        };
+
+        if (this.props.clientType === 'chromeExtension'){
+            titleStyle.linetHeight = '200%';
+        }
+
+        let buttonStyle = {
+            display: 'inline-block', 
+            height: '100%', 
+            float: 'left'
+        };
+
+        if (this.props.clientType === 'mobile'){
+            buttonStyle = {
+                position: 'absolute',
+                right: '0px'
+            };
+        }
+
         return (
-            <div style={style}>
-                <div style={{ display: 'inline-block', height: '100%', float: 'left' }}>
+            <div style={parentDivstyle}>
+                <div style={titleStyle}>
                     <h1>Open Ownership Project</h1>
                 </div>
-                <div style={{ display: 'inline-block', height: '100%', float:'right' }}>
-                    {!this.props.show.howItWorks && <HowItWorks {...this.props}/>}
+                <div style={buttonStyle}>
+                    {!this.props.show.howItWorks && <HowItWorks {...this.props} />}
                 </div>
             </div>
         )
