b09193b1a30ffb76f012f627413fd86ecbe78988
vict0rsch <vsch@protonmail.com>
2017-11-26 17:54:41 +0100

    add chips

---
+++ react/src/components/Home/Content/Example/Chip.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
+
+const chipStyle = {
+    display: 'inline-flex',
+    marginRight: '5px',
+    marginLeft: '5px',
+    marginTop:'24px'
+};
+
+export default class MyChip extends Component {
+    constructor(props) {
+        super(props);
+        //this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        console.log(this.props)
+        this.props.handleChipClick(this.props.entity)
+    }
+
+    render() {
+        
+        const avatarNameArray = this.props.entity.name.split(' ');
+        let avatarName = '';
+        for (let subName of avatarNameArray) {
+            avatarName += subName[0].toUpperCase();
+        }
+        avatarName = avatarName.slice(0,3);
+        return (
+            <Chip
+                avatar={<Avatar>{avatarName}</Avatar>}
+                label={this.props.entity.name}
+                onClick={this.handleClick}
+                style={chipStyle}
+            />
+        )
+    }
+}
