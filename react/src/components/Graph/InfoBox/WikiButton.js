efba5e4a96777d50247de283b717bf18238e2420
vict0rsch <vsch@protonmail.com>
2017-11-27 11:53:20 +0100

    fix button without wiki

--- react/src/components/Graph/InfoBox/WikiButton.js
+++ react/src/components/Graph/InfoBox/WikiButton.js
@@ -5,10 +5,11 @@ import Button from 'material-ui/Button';
 export default class WikiButton extends Component {
     render() {
         const { classes } = this.props;
-        return this.props.entity.wiki && (
-            <Button target='_blank' color="primary" className={classes.button} href={this.props.entity.wiki_link}>
+        return this.props.entity.wiki_link ?
+            (<Button target='_blank' color="primary" className={classes.button} href={this.props.entity.wiki_link}>
                 Wikipedia &nbsp;<OpenInNew />
-            </Button>
-        )
+            </Button>)
+            :
+            ''
     }
 }
