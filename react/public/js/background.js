ba1a8eaf6931371447ca440f6778ede3c74e95cb
vict0rsch <vsch@protonmail.com>
2017-11-27 18:47:11 +0100

    GA
    Former-commit-id: 26028c306265c94b9596607437ec7dafec41d842

--- react/public/js/background.js
+++ react/public/js/background.js
@@ -1,35 +1,46 @@
 fetchData();
 updateData();
-
-$(function(){
-    localStorage["newProfile"]=false;
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-110264616-1']);
+_gaq.push(['_trackPageview']);
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+console.log('Background Analytics')
+$(function () {
+    localStorage["newProfile"] = false;
     console.log('Running Background JS');
 
-    chrome.tabs.onUpdated.addListener(function(onglet) {
+    chrome.tabs.onUpdated.addListener(function (onglet) {
         if (!isNaN(onglet)) {
-            chrome.tabs.query({active: true, lastFocusedWindow: true},
-            function(array_of_tabs) {
-                var tab = array_of_tabs[0];
-                if (tab && tab.id === onglet 
-                && sessionStorage['tab_' + tab.id +'_previous'] !== tab.url){
-                    sessionStorage['tab_' + tab.id +'_previous'] = tab.url;
-                    log_tab(tab);
-                    count_tabs()    
-                }
-            });   
+            chrome.tabs.query({ active: true, lastFocusedWindow: true },
+                function (array_of_tabs) {
+                    var tab = array_of_tabs[0];
+                    if (tab && tab.id === onglet
+                        && sessionStorage['tab_' + tab.id + '_previous'] !== tab.url) {
+                        sessionStorage['tab_' + tab.id + '_previous'] = tab.url;
+                        log_tab(tab);
+                        count_tabs()
+                    }
+                });
         };
     });
 
-    chrome.tabs.onCreated.addListener(function(tab) {
-        sessionStorage['tab_' + tab.id +'_previous'] = tab.url;
+    chrome.tabs.onCreated.addListener(function (tab) {
+        sessionStorage['tab_' + tab.id + '_previous'] = tab.url;
         log_tab(tab);
         count_tabs()
     });
 
 
-    chrome.tabs.onRemoved.addListener(function(tab){
+    chrome.tabs.onRemoved.addListener(function (tab) {
         count_tabs();
-        if (localStorage['numberTabsOpen'] === "0"){
+        if (localStorage['numberTabsOpen'] === "0") {
             localStorage['currentTabUrl'] = "";
             localStorage['currentTabTitle'] = ""
         }
