c10be20d4a7ae59abc5c04f6ce98cba66636553b
vict0rsch <vsch@protonmail.com>
2017-11-29 23:50:22 +0100

    logs

--- react/src/components/Home/Home.js
+++ react/src/components/Home/Home.js
@@ -80,6 +80,10 @@ class Home extends React.Component {
 
   
   componentDidUpdate(prevProps, prevState) {
+    console.log('');
+    console.log(prevProps.dataIsAvailable);
+    console.log(this.props.dataIsAvailable);
+    console.log('');
     if (this.props.dataIsAvailable !== prevProps.dataIsAvailable){
       if (this.props.dataIsAvailable){
         updateData(this);
