a85c5f69ad9ffc6e3d8eba3a11e2681ca9499fb9
vict0rsch <vsch@protonmail.com>
2017-11-30 16:25:05 +0100

    Add issue
    Former-commit-id: 216cd201a78695477a1ac1fea75d5ab067ffc7f4

---
+++ react/src/components/Graph/InfoBox/Issue.js
@@ -0,0 +1,81 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import Dialog, {
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    withResponsiveFullScreen,
+} from 'material-ui/Dialog';
+import Icon from 'material-ui-icons/PriorityHigh';
+
+const buttonPosition = {
+    'browser': {
+        position: 'absolute',
+        right: '15%',
+    },
+    'extension': {
+        position: 'absolute',
+        right: '33px',
+    },
+    'mobile': {
+        position: 'absolute',
+        right: '1%',
+    }
+}
+
+class ResponsiveDialog extends React.Component {
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleRequestClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        let { fullScreen } = this.props;
+        if (this.props.clientType === 'extension' || window.innerWidth > 650) {
+            fullScreen = false;
+        }
+
+        return (
+            <div style={buttonPosition[this.props.clientType]}>
+                <Button style={{ width: '10px', minWidth: '10px' }} onClick={this.handleClickOpen}><Icon style={{color: '#e68931'}}/></Button>
+                <Dialog
+                    fullScreen={fullScreen}
+                    open={this.state.open}
+                    onRequestClose={this.handleRequestClose}
+                >
+                    <DialogTitle>{this.props.translate('graph.issue.dialogTitle')}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            {this.props.translate('graph.issue.dialogContentText')}<br /><br />
+                            Google Form : <a href={this.props.translate('graph.issue.form')} target="_blank" rel="noopener noreferrer">{this.props.translate('graph.issue.form')}</a><br />
+                            Email : <a href="mailto:openownership@gmail.com" target="_blank" rel="noopener noreferrer">openownership@gmail.com</a><br /><br />
+                            {this.props.translate('graph.issue.dialogContentText2')}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleRequestClose} color="primary">
+                            {this.props.translate('graph.issue.closeButton')}
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+ResponsiveDialog.propTypes = {
+    fullScreen: PropTypes.bool.isRequired,
+};
+
+export default withResponsiveFullScreen()(ResponsiveDialog);
