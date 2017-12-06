8a30bb3add88373f71271f2a4770c3e9da66701d
vict0rsch <vsch@protonmail.com>
2017-12-06 19:24:51 +0100

    create user on submit
    Former-commit-id: 190345e866220b0e40377e7a811c928e60338d93

--- react/src/components/Auth/Login.js
+++ react/src/components/Auth/Login.js
@@ -3,16 +3,19 @@ import LoginForm from './LoginForm';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../store/defaultMapStateToProps';
 import mapDispatchToProps from '../../store/defaultMapDispatchToProps';
+import Axios from 'axios';
 
 const showResults = values => {
-    console.log(values);
-    return new Promise(resolve => {
-        setTimeout(() => {
-            // simulate server latency
-            alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
-            resolve()
-        }, 500)
-    })
+    const escaped = JSON.parse(JSON.stringify(values));
+    alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+    Axios.post("http://localhost:5000/auth/register", escaped).then(
+        (resp) => {
+            console.log(resp);
+        },
+        (err) => {
+            console.log(err);
+        }
+    )
 }
 class Login extends Component {
     render() {
