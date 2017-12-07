c4eb2d4bc630b3380eb03938ec0d46571f7ea2ea
vict0rsch <vsch@protonmail.com>
2017-12-07 18:26:34 +0100

    popup auth
    Former-commit-id: a94fe4d28c1934aca0986d7e287f07b44d5eaa38

---
+++ react/src/components/Auth/Auth.js
@@ -0,0 +1,74 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import mapStateToProps from '../../store/defaultMapStateToProps';
+import mapDispatchToProps from '../../store/defaultMapDispatchToProps';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  withMobileDialog,
+} from 'material-ui/Dialog';
+import Login from './Login'
+import Icon from 'material-ui-icons/HelpOutline';
+import Button from 'material-ui/Button';
+
+
+const fullScreenMinWidth = 650;
+
+const buttonDivPositionStyle = {
+  position: 'absolute',
+  right: '24px'
+};
+const buttonStyle = {
+  width: '10px',
+  minWidth: '10px'
+};
+
+class Auth extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleRequestClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    let { fullScreen } = this.props;
+    if (this.props.clientType === 'extension' || window.innerWidth > fullScreenMinWidth) {
+      fullScreen = false;
+    }
+
+    return (
+      <div style={buttonDivPositionStyle}>
+        <Button style={buttonStyle} onClick={this.handleClickOpen}><Icon /></Button>
+        <Dialog
+          fullScreen={fullScreen}
+          open={this.state.open}
+          onRequestClose={this.handleRequestClose}
+        >
+          <DialogTitle>{this.props.translate('home.intro.dialogButton')}</DialogTitle>
+          <DialogContent component={'div'}>
+              <Login {...this.props} close={this.handleRequestClose}/>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+Auth.propTypes = {
+  fullScreen: PropTypes.bool.isRequired,
+};
+
+const _Auth = connect(mapStateToProps, mapDispatchToProps)(Auth);
+
+export default withMobileDialog()(_Auth);
