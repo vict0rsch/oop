e2aaca452ec2f7d356854c761f8d0e67097392fd
vict0rsch <vsch@protonmail.com>
2017-12-13 14:45:45 +0100

    cwm

--- react/src/components/Home/Content/Example/Example.js
+++ react/src/components/Home/Content/Example/Example.js
@@ -66,10 +66,11 @@ export default class Example extends Component {
             })
         }
     }
+    componentWillMount() {
+        console.log('CWM')
+    }
 
-
-    render() {
-        console.log('render')        
+    render() {    
         return (
             <div>
                 {this.props.translate('home.example')} <br />
