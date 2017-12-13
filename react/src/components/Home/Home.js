78cab8fd8eb9dfd30dc127649878dcba9ce2f098
vict0rsch <vsch@protonmail.com>
2017-12-13 14:43:38 +0100

    render

--- react/src/components/Home/Home.js
+++ react/src/components/Home/Home.js
@@ -113,7 +113,6 @@ class Home extends React.Component {
 
   
   render() {
-    console.log('render')
     return (
       <div>
         <Header {...this.props} style={homeContentDivStyle[this.props.clientType]} />
