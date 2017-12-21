d6920c7e85688a4f36e705016fea9fb3cb526d96
vict0rsch <vsch@protonmail.com>
2017-12-21 15:37:14 +0100

    prettify

--- react/src/components/Utils/HelpIcon.js
+++ react/src/components/Utils/HelpIcon.js
@@ -1,61 +1,36 @@
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
 import Icon from 'react-icons/lib/md/help-outline';
-import ClearIcon from 'react-icons/lib/md/clear';
-import Popover from 'material-ui/Popover';
-import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
 import { withStyles } from 'material-ui/styles';
 import Tooltip from 'material-ui/Tooltip';
-import Button from 'material-ui/Button';
+
 
 const styles = theme => ({
-    button: {
-        marginBottom: theme.spacing.unit * 4,
-    },
-    typography: {
-        padding: theme.spacing.unit * 2,
-        margin: 0,
-        backgroundColor: 'rgb(237, 234, 232)',
-        width: '200px'
-    },
     icon: {
         height: '30px',
         width: '30px',
         verticalAlign: 'middle'
+    },
+    tooltip: {
+        opacity: 0.95
     }
 });
 
+const titleStyle = {
+    fontSize: '1.5em',
+    padding: 4,
+}
+
 class HelpIcon extends Component {
-
-    state = {
-        open: false,
-        anchorEl: null,
-
-    }
-
-    button = null;
-
-    handleClickButton = () => {
-        this.setState({
-            open: true,
-            anchorEl: findDOMNode(this.button),
-        });
-    };
-
-    handleClose = () => {
-        this.setState({
-            open: false,
-        });
-    };
 
     render() {
         return (
             <Tooltip
-                id="tooltip-HomeButton"
-                title={<div style={{ fontSize: '1.5em', padding: 4 }}>{this.props.content}</div>}
+                id={"tooltip-HelpIcon-" + this.props.id}
+                title={<div style={titleStyle}>{this.props.content}</div>}
                 placement="left"
-                style={{ textAlign: 'center', fontSize: '1.2em', color: 'grey' }}
+                style={{ textAlign: 'center', fontSize: '1.2em', color: 'lightgrey' }}
+                leaveDelay={250}
+                classes={{ tooltip: this.props.classes.tooltip }}
             >
                 <Icon />
             </Tooltip>
